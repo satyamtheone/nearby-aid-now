59e179b112a470f8d0bf44a20f6d3c20b0c28ee2
satyamtheone <satyamsingh7417@gmail.com>
2025-07-13 11:29:32 +0530

    android added

---
+++ android/app/src/main/java/com/localhelp/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.localhelp.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.localhelp.app',
+  appName: 'Local Help',
+  webDir: 'dist'
+};
+
+export default config;
